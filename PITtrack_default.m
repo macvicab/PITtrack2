fbd4ae2ccedb9dfb50ea7c0538167c325165cf50
macvicab <bmacvicar@uwaterloo.ca>
2022-07-21 07:15:42 -0700

    Update PITtrack_default.m
    changed to reflect directory structure of depository.  Note that as a user you should alter this file to match your data.

--- PITtrack_default.m
+++ PITtrack_default.m
@@ -9,15 +9,9 @@ def_data.writeTravelLength = 1;
 % site 2 = Ganny = 0; % Wilket Creek (Papangelakis et al 2019)
 
 if site == 1
-    % for Bradley
-    def_data.D50 = 55; % mm
-    def_data.szcat = 2.^[5:1:6];
-    def_data.fdir = 'F:\Research\Publications\In Preparation\2020 Elli Nature Geoscience\Bradley 2017 data\tagged\';
-    def_data.fchar = 'Tag Characteristics Halfmoon.csv';
-elseif site == 2
     def_data.D50 = 37; % mm
     def_data.szcat = 2.^[5:1:8];
-    def_data.fdir = 'F:\Research\Publications\In Preparation\2020 Elli Nature Geoscience\Elli Thesis Data\Ganny\';
+    def_data.fdir = 'Ganatsekaigon\';
     def_data.fchar = 'Tag Characteristics 2.csv';
     def_data.thalname = 'GCrk_Thalweg_rev.csv';
     def_data.taglist = 'DS_Ganny_tagnum.csv';
